8a8b752b87eacaafbaa596cfa442d28912829423
Egor Erokhov <egor.erokhov1976@mail.ru>
2023-02-08 12:11:48 +0300

    Example003_Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numberA = 31;
+int numberB = 5;
+int result = numberA +numberB;
+Console.WriteLine(result);
+
+int numberC = 3;
+int numberD = 5;
+Console.WriteLine(numberC + numberD);
+
